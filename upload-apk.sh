4cf9b31fdd7da9f70f20ffdecd4f49f8be34222c
Padmal <CloudyPadmal@users.noreply.github.com>
2018-07-26 15:12:10 +0530

    Update upload-apk.sh

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,39 +1,66 @@
-#!/usr/bin/env bash
-#create a new directory that will contain out generated apk
-mkdir $HOME/buildApk/ 
-#copy generated apk from build folder and README.md to the folder just created
-cp -R app/build/outputs/apk/debug/app-debug.apk app/build/outputs/apk/release/app-release-unsigned.apk $HOME/buildApk/
-cp -R app/build/outputs/apk/debug/output.json $HOME/buildApk/debug_output.json
-cp -R app/build/outputs/apk/release/output.json $HOME/buildApk/release_output.json
-cp -R README.md $HOME/buildApk/
+#!/bin/sh
+set -e
+
+export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
+export DEVELOPMENT_BRANCH=${DEVELOPMENT_BRANCH:-travischeck}
 
 #setup git
-cd $HOME
 git config --global user.email "noreply@travis.com"
 git config --global user.name "Travis CI" 
-#clone the repository in the buildApk folder
-git clone --quiet --branch=apk https://fossasia:$GITHUB_API_KEY@github.com/fossasia/pslab-android apk > /dev/null
 
+#clone the repository into a folder named apk
+git clone --quiet --branch=apk https://CloudyPadmal:$GITHUB_API_KEY@github.com/CloudyPadmal/travispslabandroid apk > /dev/null
+echo "How outside folder looks like ....................."
+ls -h
+echo "Going into apk folder ....................."
 cd apk
-rm -rf *
-cp -Rf $HOME/buildApk/*  ./
+echo "Copying build files into apk folder ....................."
+\cp -r ../app/build/outputs/apk/*/**.apk .
+\cp -r ../app/build/outputs/apk/debug/output.json debug-output.json
+\cp -r ../app/build/outputs/apk/release/output.json release-output.json
+\cp -r ../README.md .
+echo "Current remotes are ....................."
+git remote -v
+# Signing Apps
+git remote add padmals https://github.com/CloudyPadmal/travispslabandroid.git
+echo "Added remote ....................."
 
-git checkout --orphan workaround
-git add -A
-
-#add files
-#git add -f .
-#commit and skip the tests
-
-git commit -am "Travis build pushed [skip ci]"
-
-git branch -D apk
-git branch -m apk
-
-#push to the branch apk
-git push origin apk --force --quiet> /dev/null
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
-then 
-curl https://$APPETIZE_API_TOKEN@api.appetize.io/v1/apps/4eqye6ea422e5np0gp2jfpemgm -H 'Content-Type: application/json' -d '{"url":"https://github.com/fossasia/pslab-android/blob/apk/app-debug.apk", "note": "PSLab Android App Update"}' 
+if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
+    echo "Push to master branch detected, signing the app..."
+    mv app-debug.apk app-debug-master.apk
+    cp app-release-unsigned.apk app-release-unaligned.apk
+    jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
+    \rm -f app-release.apk
+    ${ANDROID_HOME}/build-tools/27.0.3/zipalign -v -p 4 app-release-unaligned.apk app-release.apk
+    git checkout --orphan workaround
+    git add -A
+    #commit
+    git commit -am "Travis build pushed to master [skip ci]"
+    git branch -D apk
+    git branch -m apk
+    #push to the branch apk
+    git push padmals apk --force --quiet> /dev/null
 fi
+
+if [ "$TRAVIS_BRANCH" == "$DEVELOPMENT_BRANCH" ]; then
+    echo "Push to travischeck branch detected ..."
+    echo "This is how the folder looks now ................................"
+    ls -l
+    mv app-debug.apk app-debug-devel.apk
+    cp app-debug-devel.apk app-debug-${TRAVIS_PULL_REQUEST}.apk
+    echo "This is how the folder looks after ................................"
+    ls -la
+    # Checkout to branch
+    git checkout apk
+    git add -A
+    # Commit APK
+    git commit -am "[Auto] Update Test Apk ($(date +%Y-%m-%d.%H:%M:%S))"
+    
+    # Delete the existing apk branch
+    #git branch -D apk
+    # Move orphan branch stuff to new apk branch
+    #git branch -m apk
+    #git config user.name "CloudyPadmal" 
+    git push padmals apk --force --quiet> /dev/null
+fi
+
